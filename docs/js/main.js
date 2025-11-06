59a249f4f3613aca9e80020c5d721657cb689d61
Alisa <aignatina@gmail.com>
2025-11-05 22:50:09 -0500

    1 2

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,9 @@
 // declare the map variable here to give it a global scope
 let myMap;
 
+
+
+
 // we might as well declare our baselayer(s) here too
 const CartoDB_Positron = L.tileLayer(
 	'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
@@ -8,12 +11,21 @@ const CartoDB_Positron = L.tileLayer(
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 	}
 );
+const Stadia_Watercolor = L.tileLayer(
+	'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg',
+	{
+		attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap contributors.'
+	}
+);
+
+
 
 //add the basemap style(s) to a JS object, to which you could also add other baselayers. This object is loaded as a basemap selector as seen further down
 let baseLayers = {
-	"CartoDB": CartoDB_Positron
-	//,...
+	"CartoDB": CartoDB_Positron,
+	"Stadia Watercolor": Stadia_Watercolor
 };
+
 
 function initialize(){
     loadMap();
@@ -33,6 +45,42 @@ function loadMap(){
 	let lcontrol = L.control.layers(baseLayers);
 	//add the widget to the map
 	lcontrol.addTo(myMap);
+
+	fetchData("https://raw.githubusercontent.com/brubcam/GEOG-464_Lab-7/main/DATA/train-stations.geojson");
 };
+
+function fetchData(url) {
+	//load the data
+	fetch(url)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (json) {
+			//create a Leaflet GeoJSON layer using the fetched json and add it to the map object
+			L.geoJson(json, { style: styleAll, pointToLayer: generateCircles }).addTo(myMap);
+		})
+};
+
+function generateCircles(feature, latlng) {
+	return L.circleMarker(latlng);
+}
+
+function styleAll(feature) {
+	let styles = {
+		dashArray: null,
+		dashOffset: null,
+		lineJoin: null,
+		lineCap: null,
+		stroke: false,
+		color: '#000',
+		opacity: 1,
+		weight: 1,
+		fillColor: '#fff',
+		fillOpacity: 0.5,
+		radius: 9
+	};
+	return styles;
+}
+
 
 window.onload = initialize();
